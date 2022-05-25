e0608695114cabe9579ffdac3444ee7e0d499caf
MarcOrfilaCarreras <marcorfila2003@gmail.com>
2022-05-25 10:33:24 +0200

    Added the possibility to download the images from the web.

--- app/modules/add.py
+++ app/modules/add.py
@@ -1,5 +1,6 @@
 import json
 from flask import redirect, render_template, request
+import requests
 
 def mainpage():
     return render_template("new.html")
@@ -9,6 +10,12 @@ def addApp():
     with open("app/config.json", 'r') as f:
         links_dict = json.load(f)
 
+    response = requests.get(request.form["image"])
+
+    file = open("app/static/img/" + request.form["name"] + ".png", "wb")
+    file.write(response.content)
+    file.close()
+
     counter = 0
     found = False
     for group in links_dict:
@@ -16,7 +23,7 @@ def addApp():
 
             found = True
 
-            links_dict[counter]["apps"].append({"name":request.form["name"], "image":request.form["image"], "url":request.form["url"]})
+            links_dict[counter]["apps"].append({"name":request.form["name"], "image":"static/img/" + request.form["name"] + ".png", "url":request.form["url"]})
 
             with open("app/config.json", 'w', encoding='utf-8') as f:
                 json.dump(links_dict, f, ensure_ascii=False, indent=4)
@@ -25,8 +32,8 @@ def addApp():
     
     if found == False:
         links_dict.append({"name":request.form["group"], "apps":[]})
-        links_dict[counter]["apps"].append({"name":request.form["name"], "image":request.form["image"], "url":request.form["url"]})
-
+        links_dict[counter]["apps"].append({"name":request.form["name"], "image":"static/img/" + request.form["name"] + ".png", "url":request.form["url"]})
+        
         with open("app/config.json", 'w', encoding='utf-8') as f:
             json.dump(links_dict, f, ensure_ascii=False, indent=4)
     
