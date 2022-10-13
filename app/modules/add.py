55efe5995d45cef92cd715f947ce102f6232499b
Dedy Martadinata Supriyadi <dedyms@hotmail.com>
2022-10-13 19:33:32 +0700

    disable file check, somehow err, enable url again

--- app/modules/add.py
+++ app/modules/add.py
@@ -10,15 +10,15 @@ def addApp():
     with open("app/config.json", 'r') as f:
         links_dict = json.load(f)
 
-    try:
-        response = requests.get(request.form["image"])
-
-        file = open("app/static/icons/" + request.form["name"] + ".png", "wb")
-        file.write(response.content)
-        file.close()
-
-    except:
-        flash("The requested icon not found !")
+#    try:
+#        response = requests.get(request.form["image"].lower())
+#
+#        file = open("app/static/icons/" + request.form["image"].lower() + ".png", "r")
+#        file.write(response.content)
+#        file.close()
+#
+#    except:
+#        flash("The requested icon not found !")
 
     counter = 0
     found = False
@@ -27,18 +27,18 @@ def addApp():
 
             found = True
 
-            links_dict[counter]["apps"].append({"name":request.form["name"], "image":"static/icons/" + request.form["name"].lower() + ".png"})
+            links_dict[counter]["apps"].append({"name":request.form["name"], "image":"static/icons/" + request.form["image"].lower() + ".png",  "url":request.form["url"]})
 
             with open("app/config.json", 'w', encoding='utf-8') as f:
                 json.dump(links_dict, f, ensure_ascii=False, indent=4)
 
         counter += 1
-    
+
     if found == False:
         links_dict.append({"name":request.form["group"], "apps":[]})
-        links_dict[counter]["apps"].append({"name":request.form["name"], "image":"static/icons/" + request.form["name"].lower() + ".png"})
-        
+        links_dict[counter]["apps"].append({"name":request.form["name"], "image":"static/icons/" + request.form["image"].lower() + ".png",  "url":request.form["url"]})
+
         with open("app/config.json", 'w', encoding='utf-8') as f:
             json.dump(links_dict, f, ensure_ascii=False, indent=4)
-    
+
     return redirect("/")
